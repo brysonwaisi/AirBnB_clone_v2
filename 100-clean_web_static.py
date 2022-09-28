f8d19b058e259b8eb1b1684aa2bf178d5209cd78
brysonwaisi <brysonnyamwange@gmail.com>
2022-09-28 09:20:02 -0700

    Update Server changes

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['3.238.119.65', '3.236.150.166']
 
 
 @runs_once
